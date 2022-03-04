d1400772c0c803756d6ecbf204e1d0c116925232
ariadnaflores <ariadnafloresvilchez@gmail.com>
2022-03-04 13:30:38 -0500

    Registration screen

--- app/src/main/java/com/ariadna/taskieapp/templatemvvm/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/ariadna/taskieapp/templatemvvm/ui/register/RegisterActivity.kt
@@ -1,12 +1,29 @@
 package com.ariadna.taskieapp.templatemvvm.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.ariadna.taskieapp.R
+import com.ariadna.taskieapp.databinding.ActivityRegisterBinding
+import com.ariadna.taskieapp.templatemvvm.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        binding.buttonBack.setOnClickListener {
+            finish()
+        }
+
+        binding.textButtonLogin.setOnClickListener{
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
+        }
     }
 }
